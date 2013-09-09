3f16ed2286875d6091334583e71c587f935555d5
Michael LaCroix <michael@lacroixdesign.net>
2013-09-09 16:42:42 -0500

    IE, formatting fixes; styled form

--- assets/javascripts/signup/init.js
+++ assets/javascripts/signup/init.js
@@ -2,33 +2,38 @@
 
   var app = angular.module('estaine', ['shibuya.form.mailcheck']);
 
-  app.controller('SignupCtrl', ['$scope', '$http', function($scope, $http) {
-    $scope.user = { email: '' };
-    $scope.processing = false;
-    $scope.attempted  = false;
-    $scope.complete   = false;
+  app.controller('SignupCtrl', ['$scope', '$http',
+    function($scope, $http) {
+      $scope.user = { email: '' };
+      $scope.processing = false;
+      $scope.attempted  = false;
+      $scope.complete   = false;
 
-    $scope.clearErrors = function() {
-      $scope.attempted = false;
-      $scope.responseError = null;
-    };
+      $scope.clearErrors = function() {
+        $scope.attempted = false;
+        $scope.responseError = null;
+      };
 
-    $scope.submit = function(event, url, listID) {
-      event.preventDefault();
-      $scope.processing    = true;
-      $scope.responseError = null;
-      $scope.attempted     = true;
+      $scope.submit = function(event, url, listID) {
+        event.preventDefault();
+        $scope.attempted     = true;
 
-      var data = { list: listID, email: $scope.user.email };
-      $http.post(url, data)
-        .success(function() {
-          $scope.complete = true;
-        })
-        .error(function(data) {
-          $scope.responseError = data.msg;
-          $scope.processing    = false;
-        });
-    };
-  }]);
+        if ($scope.form.$valid) {
+          $scope.processing    = true;
+          $scope.responseError = null;
+
+          var data = { list: listID, email: $scope.user.email };
+          $http.post(url, data)
+            .success(function() {
+              $scope.complete = true;
+            })
+            .error(function(data) {
+              $scope.responseError = data.msg;
+              $scope.processing    = false;
+            });
+        }
+      };
+
+    }]);
 
 })();
